ef9ac82c9c8935d1622a02bd83394a52dbe9fbe9
jpsoaresXy <jpsoares3@gmail.com>
2020-09-03 14:18:27 -0300

    :sparkles: Pagina repo, Footer,Navegação do header

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import {
   Container,
@@ -15,37 +16,82 @@ import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
 import RandomCalendar from "../../components/RandomCalendar";
 
+import { APIUser, APIRepo } from "../../@types";
+
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
+
 const Profile: React.FC = () => {
+  const { username = 'jpsoaresXy' } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async (responses) => {
+      const [userResponse, reposResponse] = responses;
+
+      if (userResponse.status === 404) {
+        setData({ error: 'User Not found!' })
+        return;
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.50 - Math.random());
+      const slicedRepos = shuffledRepos.slice(0,6)
+
+      setData({
+        user,
+        repos: slicedRepos,
+      });
+    });
+  }, [username]);
+
+  if (data?.error) {
+    return <h1>{data.error} </h1>
+  }
+
+  if (!data?.user || !data?.repos) {
+    return <h1> Loading... </h1>
+  }
+
   const TabContent = () => (
     <div className="content">
       <RepoIcon />
       <span className="label">Repositories</span>
-      <span className="number">21</span>
+      <span className="number"> {data.user?.public_repos}</span>
     </div>
   );
 
   return (
     <Container>
       <Tab className="desktop">
-       <div className="wrapper">
-         <span className="offset"/>
-       <TabContent />
-       </div>
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
+
         <span className="line" />
       </Tab>
 
       <Main>
         <LeftSide>
           <ProfileData
-            username={"jpsoaresXy"}
-            name={"João Pedro Lima"}
-            avatarUrl={"https://avatars2.githubusercontent.com/u/59944502?v=4"}
-            followers={100}
-            following={7}
-            company={undefined}
-            location={"Taubaté-SP,Brasil"}
-            email={undefined}
-            blog={"linkedin.com/in/jp-soares/"}
+            username={data.user.login}
+            name={data.user.login}
+            avatarUrl={data.user.avatar_url}
+            followers={data.user.followers}
+            following={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.blog}
           />
         </LeftSide>
 
@@ -54,19 +100,20 @@ const Profile: React.FC = () => {
             <TabContent />
             <span className="line" />
           </Tab>
+
           <Repos>
             <h2>Random repos</h2>
 
             <div>
-              {[1, 2, 3, 4, 5, 6].map((n) => (
+              {data.repos.map((item) => (
                 <RepoCard
-                  key={n}
-                  username={"jpsoaresXy"}
-                  reponame={"be-the-hero"}
-                  description={"Teste"}
-                  language={n % 3 === 0 ? "JavaScript" : "TypeScript"}
-                  stars={0}
-                  forks={8}
+                  key={item.name}
+                  username={item.owner.login}
+                  reponame={item.name}
+                  description={item.description}
+                  language={item.language}
+                  stars={item.stargazers_count}
+                  forks={item.forks}
                 />
               ))}
             </div>
--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/jpsoaresXy"}>
+          jpsoaresXy
+        </Link>
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/jpsoaresXy/UI-clone-github"}>
+          UI-clone-github
+        </Link>
+      </Breadcrumb>
+
+      <p>Recriando a interface do github para exercitar o conhecimento.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>starts</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/jpsoaresXy/UI-clone-github"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
